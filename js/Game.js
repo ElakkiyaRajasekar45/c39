f8476d72efc1305dc8bfa4c7466add984b7590ab
Elakkiya45 <Elakkiyarajasekar45@gmail.com>
2020-11-15 15:45:53 +0530

    marking of player

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("c1",c1);
     car2 = createSprite(300,200);
+    car2.addImage("c2",c2);
     car3 = createSprite(500,200);
+    car3.addImage("c3",c3);
     car4 = createSprite(700,200);
+    car4.addImage("c4",c4);
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background(ground);
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -63,6 +69,9 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(10);
+          fill("yellow");
+          ellipse(x,y,60,60);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
@@ -78,7 +87,12 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+    if(player.distance>3860){
+      gameState=2;
+    }
     drawSprites();
+  }
+  end(){
+    console.log("GameOver");
   }
 }
